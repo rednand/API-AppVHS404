586b15071aa54f03ceccf8a50d305e29e4519943
Renan de Aguiar Modesto <renan.modesto@grupomultiplica.com.br>
2022-04-19 22:44:30 -0300

    to heroku

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ const morgan = require("morgan");
 const app = express();
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const movieRoutes = require("./src/routes/MovieRoutes");
-const castroutes = require("./src/routes/castroutes");
+const movieRoutes = require("../src/routes/MovieRoutes");
+const castroutes = require("../src/routes/castroutes");
 require("dotenv").config();
 
 app.set("view engine", "ejs");
