f0b4a3403032678bf4d67063a31b8b8fda90129a
Renan de Aguiar Modesto <Renan de Aguiar Modesto>
2022-09-14 16:18:19 -0300

    ajuste change name

--- src/server/server.js
+++ src/server/server.js
@@ -9,6 +9,7 @@ const movieRoutes = require("../routes/MovieRoutes");
 const castroutes = require("../routes/castroutes");
 require("dotenv").config();
 
+
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.use(express.static("views"));
